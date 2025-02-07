6b7630042ef3c707652d860e264dabc416e7843d
Haroon <haroon@example.com>
2025-02-07 17:41:28 -0500

    Add GitHub Actions workflow with database migrations and update database configuration

--- blinkea/settings.py
+++ blinkea/settings.py
@@ -99,11 +99,13 @@ WSGI_APPLICATION = 'blinkea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
