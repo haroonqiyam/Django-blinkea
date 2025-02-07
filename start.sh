ea690ad19960a79b150bbe6aad6e6567f0329de6
Haroon <haroon@example.com>
2025-02-07 13:46:34 -0500

    Added database data migration
    - Added data dump from local database
    - Updated start script to load initial data during container startup

--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@
 echo "Applying database migrations..."
 python manage.py migrate
 
+# Load initial data if database is empty
+echo "Checking and loading initial data if needed..."
+python manage.py loaddata data.json
+
 # Start Gunicorn
 echo "Starting Gunicorn..."
 gunicorn --bind :$PORT blinkea.wsgi:application
